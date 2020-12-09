3f27fbe44f08fed1e20080c7c0956279112c2bb8
r4hr <info@d4hr.com>
2020-12-09 07:12:17 -0300

    Actualiza código con comentarios

--- datos_rh_vis_storytelling.R
+++ datos_rh_vis_storytelling.R
@@ -1,6 +1,6 @@
-library(tidyverse)
-library(scales)
-theme_set(theme_minimal())
+library(tidyverse)          # Transformación y limpieza de datos
+library(scales)             # Manipula formatos de número
+theme_set(theme_minimal())  # Setea el estilo de los gráficos                                                                             
 
 # Cargar archivos -------------------------------
 
@@ -18,6 +18,7 @@ kpi_long <- kpi %>%
 encuesta <- read_delim("https://raw.githubusercontent.com/r4hr/r4hr_introduccion_dplyr/main/Datos/encuesta_sueldos.csv",
                        delim = ",")
 
+# Organiza la jerarquía en los puestos de la encuesta.
 encuesta <- encuesta %>% 
   mutate(puesto = factor(puesto, levels = c("Director", "Gerente", "Jefe",
                                             "Responsable", "HRBP", "Analista",
@@ -38,15 +39,15 @@ kpi_long <- kpi_long %>%
 
 
 
-# Gráfico de líneas
-ggplot(kpi_long, aes(x = Mes, y = Tasa, group = KPI, color = Sector)) +
-  geom_line(size = 1)
+# Gráfico de líneas según la grámatica de los gráficos
 
-
-ggplot(kpi_long, aes(x = Mes, y = Tasa, group = KPI, color = KPI)) +
-  geom_line(size = 1) +
-  facet_wrap(~Sector) +
-  theme_minimal()
+ggplot(kpi_long,               # Fuente de datos a graficar
+       aes(x = Mes,            # Variable del eje horizontal
+           y = Tasa,           # Variable del eje vertical
+           group = KPI,        # Criterio para agrupar las líneas del gráfico
+           color = KPI)) +     # Define el color de la línea
+  geom_line(size = 1) +        # Tipo de gráfico y grosor de la línea
+  facet_wrap(~Sector)          # Crea subgráficos divididos por sector
 
 
 
@@ -55,44 +56,72 @@ ggplot(kpi_long, aes(x = Mes, y = Tasa, group = KPI, color = KPI)) +
 
 # Etiquetas de los ejes -------------------------
 
+# Creamos un dataframe para calcular los sueldos promedios por puestos
 
-sueldos_puestos <- encuesta %>% 
-  select(puesto, sueldo_bruto) %>% 
-  group_by(puesto) %>% 
-  summarise(sueldo_promedio = mean(sueldo_bruto))
-
-ggplot(sueldos_puestos, aes(x = puesto, y = sueldo_promedio)) +
-  geom_col(fill = "#5B61DC") #+
-#  scale_y_continuous(labels = comma_format(big.mark = ".", decimal.mark = ",")) +
-# labs(x="", y = "", title = "Sueldo promedio por puesto")
+sueldos_puestos <- encuesta %>%    # Crea un dataframe nuevo llamado sueldos_puestos
+  select(puesto, sueldo_bruto) %>% # Elige las variables para trabajar
+  group_by(puesto) %>%             # Agrupa los resultados por puestos
+  summarise(sueldo_promedio = mean(sueldo_bruto)) # Cálculo de sueldo promedio
 
 
-sueldos_rubros <- encuesta %>% 
-  select(rubro, sueldo_bruto) %>% 
-  group_by(rubro) %>% 
-  summarise(sueldo_promedio = mean(sueldo_bruto),
-            cantidad = n()) %>% 
-  arrange(-cantidad) %>% 
+
+# Gráfico sin separador de miles en eje vertical y nombres originales de los ejes
+
+ggplot(sueldos_puestos,               # Fuente de datos a graficar
+       aes(x = puesto,                # Variable del eje horizontal
+           y = sueldo_promedio)) +    # Variable del eje vertical
+  geom_col(fill = "#5B61DC")          # Define el tipo de gráfico y el color
+
+
+
+# Mismo gráfico, con separador miles y sin nombres originales
+
+ggplot(sueldos_puestos,               # Fuente de datos a graficar
+       aes(x = puesto,                # Variable del eje horizontal
+           y = sueldo_promedio)) +    # Variable del eje vertical
+  geom_col(fill = "#5B61DC")  +       # Define el tipo de gráfico y el color
+# Agrega el separador de miles al eje y
+ scale_y_continuous(labels = comma_format(big.mark = ".", decimal.mark = ",")) +
+ labs(x="", y = "",                   # Elimina los nombres originales de las variables
+      title = "Sueldo promedio por puesto") # Agrega un título
+
+
+
+
+# Creamos un dataframe para calcular los sueldos promedio por rubro
+sueldos_rubros <- encuesta %>%         # Crea un dataframe nuevo llamado sueldos_rubros
+  select(rubro, sueldo_bruto) %>%      # Selecciona las variables rubro y sueldo_bruto
+  group_by(rubro) %>%                  # Agrupa por rubro
+  summarise(sueldo_promedio = mean(sueldo_bruto),   # Calcula sueldo promedio
+            cantidad = n()) %>%        # Agrega una columna con cantidad de casos
+  arrange(-cantidad) %>%               # Ordena descendentemente las respuestas
+  # Cambia el nombre de algunas respuestas para que sean más cortas
   mutate(rubro = fct_recode(rubro, "IT" = "Tecnologías de Información, Sistemas, y afines",
                             "Servicios financieros" = "Servicios financieros; seguros",
                             "Terminales automotrices" = "Terminales automotrices, fábricas autopartistas, y afines")) %>% 
-  filter(rubro != "Otros") %>% 
-  top_n(10)
+  filter(rubro != "Otros") %>%        # Elimina las respuestas "Otros" del campo rubro
+  top_n(10)                           # Nos quedamos con el top 10 de respuestas
 
-sueldos_rubros
 
-# Gráfico base
-grafico_rubros <- ggplot(sueldos_rubros, aes(x = reorder(rubro, sueldo_promedio),
-                                             y = sueldo_promedio)) +
-  geom_col(fill = "#5B61DC") + labs(x = "", y = "") +
+
+# Guardamos en un objeto el gráfico base así simplificamos el código despues
+grafico_rubros <- ggplot(sueldos_rubros,                           # Fuente de datos a graficar
+                         aes(x = reorder(rubro, sueldo_promedio),  # Datos en el eje horizontal ordenados por sueldo_promedio
+                                             y = sueldo_promedio)) + # Datos en el eje vertical
+  geom_col(fill = "#5B61DC")      # Tipo de gráfico y color de relleno
++ labs(x = "", y = "") +          # Elimina nombres originales de los ejes
+  # Agrega separador de miles al eje vertical
   scale_y_continuous(labels = comma_format(big.mark = ".", decimal.mark = ","))
 
 
-# Etiquetas rotadas
+# Gráfico base, con los nombres de las etiquetas del eje x superpuestos
+grafico_rubros
+
+# Gráficos con etiquetas rotadas
 grafico_rubros +
   theme(axis.text.x = element_text(angle = 90))
 
-# Etiquetas solapadas
+# Gráfico con etiquetas escalonadas
 grafico_rubros +
   scale_x_discrete(guide = guide_axis(n.dodge = 2))
 
